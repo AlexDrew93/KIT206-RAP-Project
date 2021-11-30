f0bac4542cb27029dcd740c473a0c0142ec5f769
Alex Drew <adrew543@gmail.com>
2021-11-30 11:11:33 +1100

    Reformatted file structure to include Employee class file

---
+++ KIT206-RAP-Project/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial5
+{
+    enum Gender
+    {
+        M,
+        F,
+        X
+    }
+    class Employee
+    {
+
+
+        public string name;
+        public int id;
+        public Gender gender;
+
+        public override string ToString()
+        {
+            return name + " " + id + " " + gender;
+        }
+
+
+    }
+}
