5a94669f708fbdad951cf37a3d14532255d3c951
Babbage666 <94875874+Babbage666@users.noreply.github.com>
2021-12-06 07:09:21 +1100

    Added rough draft Staff and Student
    Staff and Student as child classes of Researcher. Note: the namespaces need to be separate for Controller, Data and View, I haven't done this yet.

---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_RAP_Project
+{
+
+    public class Student : Researcher
+    {
+   
+        private string degree; 
+
+        public string Degree
+        {
+            get
+            {
+                return givenname;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    givenname = value;
+                }
+            }
+        }
+
+    }
+}
